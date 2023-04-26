255caf751a6c684f25e6acc753fa6508f1369548
mufeedppm <mufeedppm@gmail.com>
2023-04-26 09:32:59 +0530

    signup backend api added

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const sequelize = require('./utility/database');
+
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/user',userRoutes);
+
+sequelize.sync().then(()=>{
+    app.listen(3000)
+}).catch(err => console.log(err) );
